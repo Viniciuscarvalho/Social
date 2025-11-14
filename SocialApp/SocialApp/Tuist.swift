4ecee2d63beca23f0a45bbdc2d16532753c1b034
Vinicius Carvalho <viniciuscarvalho789@gmail.com>
2025-11-14 19:03:24 -0300

    Reestructure tuist

--- SocialApp/SocialApp/Tuist.swift
+++ SocialApp/SocialApp/Tuist.swift
@@ -1,3 +1,11 @@
 import ProjectDescription
 
-let tuist = Tuist(fullHandle: "viniciuscarvalho789/SocialApp", project: .tuist())
+let tuist = Tuist(
+    fullHandle: "viniciuscarvalho789/SocialApp",
+    project: .tuist(
+        generationOptions: .options(
+            resolveDependenciesWithSystemScm: true,
+            disablePackageVersionLocking: true
+        )
+    )
+)
